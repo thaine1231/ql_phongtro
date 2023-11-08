1750ebefaa64718465ba55d97a16d52090652a58
Anh Anh <ngovietanh121@gmail.com>
2023-11-08 11:00:41 +0700

    Test2
    upload file lên git

---
+++ src/ql_phongtro/Ql_phongtro.java
@@ -0,0 +1,20 @@
+/*
+ * Hello This Is Vanh
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ql_phongtro;
+
+/**
+ *
+ * @author Chanh Chanh
+ */
+public class Ql_phongtro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
